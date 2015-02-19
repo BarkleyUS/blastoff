ea098387f77581797513d14e569cfd5e7d46c0c7
Seth MacPherson <smacpherson@barkleyus.com>
2015-02-19 14:29:11 -0600

    Adding (ROUGH) functionallity into blastoff launch

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-var color = require('colors');
-
-var fs = require("fs"),
-path = require("path");
+var color = require('colors'),
+    fs = require("fs"),
+    path = require("path"),
+    rsync = require("rsync");
 
 var args = process.argv.slice(2);
 
@@ -20,24 +20,38 @@ if (typeof(args[0]) == "undefined" || args[0] == "help") {
 } else {
     if (args[0].toLowerCase() === "launch") {
 
-        // process.stdout.write('\033c');
+        process.stdout.write('\033c');
 
         console.log('Launching Blastoff');
 
-        //
-        // function countdown(number) {
-        //     if (number > 0) {
-        //         console.log(number + '...');
-        //         setTimeout(countdown(--number), 2000);
-        //     }
-        // }
-        //
-        // setTimeout(countdown(5), 2000);
 
-    }
+        function countdown(number) {
+            if (number > 0) {
+                console.log(number + '...');
+                setTimeout(function() {countdown(--number)}, 500);
+            } else {
+                (function() {
+                    copy();
+                })();
+            }
+        }
 
-    if (args[0].toLowerCase() === "path") {
-        // CURRENT DIRECTORY
-        console.log(process.cwd());
+        function copy() {
+            var copy = new rsync()
+                .shell('ssh')
+                .flags('vcr')
+                .source(__dirname + "/default/assets/")
+      .         destination(process.cwd() + "/assets/");
+
+            copy.execute(function(err, code, cmd) {
+                if (err) {
+                    console.error(err);
+                }
+                console.log('Blastoff has completed: '.green + cmd);
+            });
+        }
+
+        setTimeout(function(){ countdown(5) }, 500);
+
     }
 }
