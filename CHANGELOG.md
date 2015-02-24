65cd75fc172b4d821c12a711bc9af2a6169b011c
Seth MacPherson <smacpherson64@gmail.com>
2015-02-23 22:43:02 -0600

    Bug Fixes, Implementing functionality for windows, Updating Comments

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,14 @@
+##0.1.2 - Adding Windows Functionality
+* Fixing Copy Function to work on Windows
+* Cleaning up and commenting code
+* Updating command to start for index.js
+
+##0.1.1 - Fixing bug issues adding concept Windows Functionality
+* Updating actions.js file to have basic functionality for windows
+* Removing incorrect colors module in package.json
+* Adding correct colors module in package.json
+* Minor bug fix with running command
+
 ##0.1.0 - Initial Working State (ROUGH)
 * Updated Default Structure
 * Updated Gulpfile to be relative to current working directory
--- index.js
+++ index.js
@@ -8,9 +8,14 @@ if (typeof(args[0]) == "undefined" || args[0] == "help") {
     actions.help();
 } else {
     if (args[0].toLowerCase() === "launch") {
-        actions.launch();
+            console.log(args.join(' '));
+        if (args.join(' ').indexOf('--quick') === -1) {
+                actions.launch(false);
+        } else {
+            actions.launch(true);
+        }
     }
-    if (args[0].toLowerCase() === "go" || args[0].toLowerCase() === "watch" ) {
-        actions.go();
+    if (args[0].toLowerCase() === "start" || args[0].toLowerCase() === "go" || args[0].toLowerCase() === "watch" ) {
+        actions.start();
     }
 }
