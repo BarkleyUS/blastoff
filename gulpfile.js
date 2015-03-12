5045b9ffc3042b550c21fc5cee81d0ccb84f65d6
Seth MacPherson <smacpherson@barkleyus.com>
2015-03-12 14:53:40 -0500

    Adding vendor and bower locations into sass includePaths

--- gulpfile.js
+++ gulpfile.js
@@ -42,7 +42,7 @@ gulp.task( 'sass', function(){
 	gulp.src( assets.sass.main )
 		.pipe( plumber() )
 		.pipe( sourcemaps.init() )
-		.pipe( sass({outputStyle: 'compressed'}) )
+		.pipe( sass({outputStyle: 'compressed', includePaths: [assetsDir + '/vendor/', assetsDir + '/vendor/bower_components/']}) )
 		.pipe( autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4', 'Firefox >= 4'))
 		.pipe( sourcemaps.write('.') )
 		.pipe( gulp.dest( assetsDir + 'css/' ) );
